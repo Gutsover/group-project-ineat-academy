3d292fff89d5f30438100bf85e3404b6eb809851
Benjamin MARCHAND <bmarchand@ineat.fr>
2020-01-29 14:23:27 +0100

    Added gallery-service. TODO : move the gallery in the folder services

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDAHqW_zz-ql4f3yZyduwYLdOJQj1ksyC0",
-    authDomain: "ineat-project-group.firebaseapp.com",
-    databaseURL: "https://ineat-project-group.firebaseio.com",
-    projectId: "ineat-project-group",
-    storageBucket: "ineat-project-group.appspot.com",
-    messagingSenderId: "1039943448518",
-    appId: "1:1039943448518:web:870e6c643bcda74017e4c7",
-    measurementId: "G-LTVD9XNE8K"
+    apiKey: "AIzaSyB-tUw5dzkmqlHIkc6P_luwYxtQUATE0NM",
+    authDomain: "projet-groupe-alien3-5.firebaseapp.com",
+    databaseURL: "https://projet-groupe-alien3-5.firebaseio.com",
+    projectId: "projet-groupe-alien3-5",
+    storageBucket: "projet-groupe-alien3-5.appspot.com",
+    messagingSenderId: "179472063199",
+    appId: "1:179472063199:web:1222c3dc2aff87c602676a",
+    measurementId: "G-S17Q2ECL9X"
   }
 };
 
