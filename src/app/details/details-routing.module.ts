517f36c300720598e688da89edd1aa10e67055ce
Benjamin MARCHAND <bmarchand@ineat.fr>
2020-01-31 12:11:56 +0100

    Now you can access details module by clicking on a gallerys picture

--- src/app/details/details-routing.module.ts
+++ src/app/details/details-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DetailsComponent } from './details.component';
 
 
 const routes: Routes = [
