dd104c945e75d44e64a6a372f723b8c7d72f9574
Mylène VANDAËLE <mvandaele@ineat.fr>
2020-02-04 15:41:58 +0100

    du caca

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthenticationService } from './../../services/authentication.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,11 +11,39 @@ export class RegisterComponent implements OnInit {
 
   errorMessage = '';
   successMessage = '';
+  registerForm: FormGroup;
+
+  @Output() userRegister = new EventEmitter<any>();
+  @Output() cancelRegisterForm = new EventEmitter<any>();
 
   constructor(private authenticationService: AuthenticationService ) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    })
   }
+
+  cancel() {
+    this.cancelRegisterForm.emit();
+  }
+
+  
+  get email() {
+    return  this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+  
 
   // Méthode de création de compte
   tryRegister(value) {
